1a276087c280a477f8006c683a80f9ba44727534
John Schellinger <john.schellinger@genesys.com>
2023-03-27 23:27:45 -0400

    Use simple ref text

--- renderer/components/git-history.tsx
+++ renderer/components/git-history.tsx
@@ -1,26 +1,27 @@
-import GitRef from './git-ref';
-
 function GitHistory(props) {
   return (
     <div className='grid grid-cols-1 w-full'>
       <h1 className='text-lg text-gray-500'>{props.cwd}</h1>
       <hr />
       <ul>
-        {props.history?.data?.all?.map((item) => (
-          <li className='flex w-full p-2'>
-            <span className=''>
-              {item.refs.split(',').map((ref: string) => {
-                return <GitRef gitRef={ref}></GitRef>;
-              })}
-            </span>
-            <span className='flex grow text-ellipsis'>{item.message}</span>
-            <span className='flex-none text-ellipsis pl-2 pr-2'>{item.author_name}</span>
-            <span className='flex-none pl-2 pr-2'>{item.hash.substring(0, 8)}</span>
-            <span className='flex-none pl-2 pr-2'>
-              {new Date(item.date).toLocaleDateString('en-US')} @ {new Date(item.date).toLocaleTimeString('en-US')}
-            </span>
-          </li>
-        ))}
+        {props.history?.data?.all?.[0]?.hash.split('\n').map(
+          (item) => (
+            <li className='flex w-full'>
+              <span className='font-mono truncate'>{item}</span>
+            </li>
+          ),
+          // <li className='flex w-full p-2'>
+          //   {item.refs.split(',').map((ref: string) => {
+          //     return <GitRef gitRef={ref}></GitRef>;
+          //   })}
+          //   <span className='flex grow text-ellipsis'>{item.message}</span>
+          //   <span className='flex-none text-ellipsis pl-2 pr-2'>{item.author_name}</span>
+          //   <span className='flex-none pl-2 pr-2'>{item.hash.substring(0, 8)}</span>
+          //   <span className='flex-none pl-2 pr-2'>
+          //     {new Date(item.date).toLocaleDateString('en-US')} @ {new Date(item.date).toLocaleTimeString('en-US')}
+          //   </span>
+          // </li>
+        )}
       </ul>
       <hr />
     </div>
