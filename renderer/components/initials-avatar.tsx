710e0fe4e8c846d12feb1717a771318b3ebf3c0b
John Schellinger <john.schellinger@genesys.com>
2023-03-29 07:50:02 -0400

    Fix initials avatar styling to more consistently show a true circle

--- renderer/components/initials-avatar.tsx
+++ renderer/components/initials-avatar.tsx
@@ -1,18 +1,18 @@
 import React from 'react';
 
 function InitialsAvatar(props: InitialsAvatarProps) {
-  const initials = props.name?.split(' ')[0].charAt(0) + props.name?.split(' ')[1].charAt(0);
+  const initials = props.name.split(' ')[0].charAt(0) + props.name.split(' ')[1].charAt(0);
 
-  const seededColor = (str) => {
+  const seededColor = (name: string) => {
     let hash = 0;
-    for (let i = 0; i < str.length; i++) {
-      hash = str.charCodeAt(i) + ((hash << 5) - hash);
+    for (let i = 0; i < name.length; i++) {
+      hash = name.charCodeAt(i) + ((hash << 5) - hash);
     }
 
     let color = '#';
     for (let i = 0; i < 3; i++) {
       const value = (hash >> (i * 8)) & 0xff;
-      color += ('00' + value.toString(16)).substr(-2);
+      color += ('00' + value.toString(16)).substring(-2);
     }
 
     return color;
@@ -22,7 +22,9 @@ function InitialsAvatar(props: InitialsAvatarProps) {
 
   return (
     <React.Fragment>
-      <span className='w-fit rounded-full p-1' style={{ backgroundColor: color }}>
+      <span
+        className='w-6 h-6 flex justify-center items-center text-xs rounded-full'
+        style={{ backgroundColor: color }}>
         {initials}
       </span>
     </React.Fragment>
