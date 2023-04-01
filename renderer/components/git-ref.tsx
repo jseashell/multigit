55a528ef52bf3644f7b6e21ec88c852ffd047c71
John Schellinger <john.schellinger@genesys.com>
2023-04-01 02:56:11 -0400

    Fix branch styling

--- renderer/components/git-ref.tsx
+++ renderer/components/git-ref.tsx
@@ -8,13 +8,13 @@ function GitRef(props: GitRefProps) {
       // Collapsed remote branch (has a local counterpart)
       return (
         <span className='flex h-full border border-green-300 bg-green-900 rounded px-2 mr-1'>
-          <img className='max-w-none' width='16px' height='16px' src='/images/git-branch.svg' />
+          <img className='max-w-none' width='12px' height='12px' src='/images/git-branch.svg' />
         </span>
       );
     } else if (ref.includes('origin')) {
       // Remote branch
       return (
-        <span className='whitespace-nowrap w-fit border border-green-300 bg-green-900 rounded text-sm px-2 mr-1'>
+        <span className='whitespace-nowrap flex h-full w-fit border border-green-300 bg-green-900 rounded text-sm px-2 mr-1'>
           {ref}
         </span>
       );
@@ -22,21 +22,21 @@ function GitRef(props: GitRefProps) {
       // Local HEAD
       const branch = '\u2713 ' + ref.match(/HEAD -> (.*)/)[1];
       return (
-        <span className='whitespace-nowrap w-fit border border-red-300 bg-red-900 rounded text-sm px-2 mr-1'>
+        <span className='whitespace-nowrap flex h-full w-fit border border-red-300 bg-red-900 rounded text-sm px-2 mr-1'>
           {branch}
         </span>
       );
     } else if (ref.includes('tag')) {
       // Tag
       return (
-        <span className='whitespace-nowrap w-fit border border-blue-300 bg-blue-900 rounded text-sm px-2 mr-1'>
+        <span className='whitespace-nowrap flex h-full w-fit border border-blue-300 bg-blue-900 rounded text-sm px-2 mr-1'>
           {ref.match(/tag: (.*)/)[1]}
         </span>
       );
     } else if (ref != '') {
       // Local branch
       return (
-        <span className='whitespace-nowrap w-fit border border-red-300 bg-red-900 rounded text-sm px-2 mr-1'>
+        <span className='whitespace-nowrap flex h-full w-fit border border-red-300 bg-red-900 rounded text-sm px-2 mr-1'>
           {ref}
         </span>
       );
